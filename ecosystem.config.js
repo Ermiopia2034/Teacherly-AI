ceb450af6b2da1aa974c552a01e9731f38c63eac
Ermiyas2034 <ermiyas2034@gmail.com>
2025-07-22 13:17:04 +0300

    Fixed stupid mistake

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
       PORT: 3001,
       NODE_ENV: 'production'
     },
-    cwd: '/home/deployuser/projects/teacherly/Teacherly-AI',
+    cwd: '/home/deployuser/projects/teacherly/teacherly-ai',
     error_file: '/home/deployuser/logs/teacherly/frontend-error.log',
     out_file: '/home/deployuser/logs/teacherly/frontend-out.log',
   }]
